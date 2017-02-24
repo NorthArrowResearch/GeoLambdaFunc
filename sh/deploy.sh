f00674ee024c4a644995268670371d64b0771a71
Work <work@Matts-MacBook-Pro.local>
2017-02-24 14:16:18 -0800

    fixing the deploy

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -2,12 +2,32 @@
 # DEPLOYMENT SCRIPT
 # --------------------------------------------------
 
+HOMEDIR=${PWD}
+VIRTUAL_ENV=$HOMEDIR/env
+ZIPFILE=$HOMEDIR/Deploy.zip
+S3URL=s3://nartesting/LAMBDA/functioname/func.zip
+
+rm $ZIPFILE
+
 # Create our virtual environment
-virtualenv --no-site-packages env
+virtualenv --no-site-packages envdeac
 /usr/src/env/bin/pip install --upgrade pip
 /usr/src/env/bin/pip --timeout=120 install -r requirements.txt
 
 # These come from having pre-compiled them 
 # on this box
-cp /usr/local/lib/libgdal.so.20.1.2 .
-cp /usr/local/lib/libproj.so.0.7.0 .
+zip -9 $ZIPFILE /usr/local/lib/libgdal.so.20.1.2 .
+zip -9 $ZIPFILE /usr/local/lib/libproj.so.0.7.0 .
+
+# Add our virtualenv libraries
+cd $VIRTUAL_ENV/lib/python2.7/site-packages
+zip -r9 $ZIPFILE *
+cd $VIRTUAL_ENV/lib64/python2.7/site-packages
+zip -r9 $ZIPFILE *
+
+# Now add our python code
+cd $HOMEDIR
+zip -r9 $ZIPFILE *.py
+
+# Now for the actual deploy
+aws s3 cp $ZIPFILE $S3URL
